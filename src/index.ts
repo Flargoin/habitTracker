b6bf2a5cecc4512eaf9b409a2ab69d2bd7fe6290
Sergey <flargoin@gmail.com>
2025-08-28 23:20:48 +0300

    1 day. Install sass, added blocks for habbit

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import './styles/style.css';
+import "./styles/style.scss";
 
 class App {
   constructor() {
-    this.init();
+    // this.init();
   }
 
-  private init(): void {
-    console.log('App initialized with TypeScript!');
-    this.renderContent();
-  }
+  // private init(): void {
+  //   console.log('App initialized with TypeScript!');
+  //   this.renderContent();
+  // }
 
-  private renderContent(): void {
-    const appElement = document.getElementById('app');
-    if (appElement) {
-      appElement.innerHTML += `
-        <div>
-          <p>Welcome to TypeScript Webpack setup!</p>
-          <button id="clickMe">Click me!</button>
-        </div>
-      `;
+  // private renderContent(): void {
+  //   const appElement = document.getElementById('app');
+  //   if (appElement) {
+  //     appElement.innerHTML += `
+  //       <div>
+  //         <p>Welcome to TypeScript Webpack setup!</p>
+  //         <button id="clickMe">Click me!</button>
+  //       </div>
+  //     `;
 
-      const button = document.getElementById('clickMe');
-      if (button) {
-        button.addEventListener('click', () => {
-          alert('Button clicked! Webpack is working!');
-        });
-      }
-    }
-  }
+  //     const button = document.getElementById('clickMe');
+  //     if (button) {
+  //       button.addEventListener('click', () => {
+  //         alert('Button clicked! Webpack is working!');
+  //       });
+  //     }
+  //   }
+  // }
 }
 
 // Запуск приложения
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,14 @@
-// webpack.config.js
 const path = require("path");
-const Dotenv = require("webpack-dotenv-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
 
   return {
     entry: "./src/index.ts",
-
-    // mode будет установлен из CLI аргументов
     mode: argv.mode || "development",
-
-    // Разные source maps для разных режимов
     devtool: isProduction ? "source-map" : "inline-source-map",
 
     devServer: {
@@ -31,48 +26,82 @@ module.exports = (env, argv) => {
 
     module: {
       rules: [
+        // TypeScript rule
         {
           test: /\.tsx?$/,
           use: "ts-loader",
           exclude: /node_modules/,
         },
+
+        // Sass/SCSS rule
+        {
+          test: /\.(scss|sass)$/i,
+          use: [
+            // В production извлекаем CSS в отдельные файлы
+            // В development используем style-loader для горячей перезагрузки
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+
+        // CSS rule (если еще используете обычный CSS)
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
+        },
+
+        // Images and fonts
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[hash][ext][query]",
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "fonts/[hash][ext][query]",
+          },
         },
       ],
     },
 
     resolve: {
-      extensions: [".tsx", ".ts", ".js"],
+      extensions: [".tsx", ".ts", ".js", ".scss"],
     },
 
     plugins: [
-      new Dotenv({
-        sample: "./.env.example",
-        path: "./.env",
-      }),
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: "./src/index.html",
-        title: "TypeScript Webpack App",
-        minify: isProduction
-          ? {
-              removeComments: true,
-              collapseWhitespace: true,
-              removeRedundantAttributes: true,
-            }
-          : false,
+        title: "TypeScript + Sass App",
+        minify: isProduction,
       }),
+
+      // Extract CSS только в production
+      ...(isProduction
+        ? [
+            new MiniCssExtractPlugin({
+              filename: "[name].[contenthash].css",
+              chunkFilename: "[id].[contenthash].css",
+            }),
+          ]
+        : []),
     ],
 
     output: {
       filename: isProduction ? "[name].[contenthash].js" : "[name].js",
       path: path.resolve(__dirname, "dist"),
       clean: true,
+      assetModuleFilename: "assets/[hash][ext][query]",
     },
 
-    // Оптимизации только для production
     optimization: {
       minimize: isProduction,
       splitChunks: {
@@ -82,6 +111,12 @@ module.exports = (env, argv) => {
             test: /[\\/]node_modules[\\/]/,
             name: "vendors",
             chunks: "all",
+          },
+          styles: {
+            test: /\.(css|scss|sass)$/,
+            name: "styles",
+            chunks: "all",
+            enforce: true,
           },
         },
       },
